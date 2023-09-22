04abb76da7796e2ebe8789075130cd3670a60283
DESKTOP-FIM6H62\USER <0721juli@naver.com>
2023-09-22 13:24:34 +0900

    회원가입시 전화번호 입력 추가

--- models/users.js
+++ models/users.js
@@ -14,6 +14,11 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
     account_number: {
       type: DataTypes.INTEGER,
       allowNull: false,
