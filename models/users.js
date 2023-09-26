22e07cb5add35dede92a2d76e2e1de15b2223521
RaseBerrie <yjin2006@gmail.com>
2023-09-26 11:13:15 +0900

    멤버십 페이지 (아직 버튼은 안 눌립니다.)

--- models/users.js
+++ models/users.js
@@ -38,6 +38,10 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
+    },
+    membership: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   }, {
     timestamps: false
