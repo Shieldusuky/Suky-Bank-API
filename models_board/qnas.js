7eea615e7ff4aebb4e7e52d9de71a10f592598ba
DESKTOP-FIM6H62\USER <hwanghsun@naver.com>
2023-10-08 21:07:22 +0900

    2. QnA getBoard, editBoard 추가

--- models_board/qnas.js
+++ models_board/qnas.js
@@ -17,7 +17,7 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: true
         },
-        createAt: {
+        createdAt: {
             type: DataTypes.DATE,
             allowNull: true
         },
